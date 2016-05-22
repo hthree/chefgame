1fa7accd54b93079d9884b2282a5e11b5673b649
hthree <johnwheino@gmail.com>
2016-05-22 15:40:55 -0700

    changed slightly

--- templates/index.php
+++ templates/index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <div style ="width:100%;height:100%">
-<div style="width:500px;height:400px;align:center;">
+<div style="width:750px;height:400px;align:center;">
 <?php
 
 // Just grab whatever data was stored in the template
@@ -16,23 +16,23 @@ foreach($rec as $step){
 echo "</div><div style=\"width:250px;height:400px;float:left;\">";
 echo "<div style=\"width:250px;height:300px;overflow:auto;float:left;\" id=\"jac\">";
 echo "</div><div style=\"width:250px;height:100px;float:left;\">";
-echo "<div style=\"width:125px;height:100px;float:left;\" onclick = \"clickIng()\">add ingredient</div>";
-echo "<div style=\"width:125px;height:100px;float:left;\" onclick = \"clickMet()\">add step</div>";
+echo "<div style=\"width:250px;height:100px;float:left;\" id=\"mic\" onclick = \"clickMet()\">add Step</div>";
+//6zjv193
+
+echo "<div style=\"width:250px;height:300px;overflow:auto;float:left;\" id=\"tic\">";
+echo "</div><div style=\"width:250px;height:100px;float:left;\">";
+echo "<div style=\"width:250px;height:100px;float:left;\" id=\"mic\" onclick = \"clickIng()\">add ingredient</div>";
 echo "</div></div>";
 	
 echo "<script>\n"; 
 //echo"document.getElementById(\"ing\").onclick = function() {clickIng()};document.getElementById(\"met\").onclick = function() {clickMet()};";
-echo "function clickIng() {var div = document.getElementById(\"jac\"); var hid = document.createElement(\"div\"); hid.setAttribute(\"style\",\"float:left;width:230px;height:100px\"); var form = document.createElement(\"form\"); form.setAttribute(\"method\",\"post\"); form.setAttribute(\"action\",\"http://jheino.rop.launchbrigade.com/infotech/\"); var select = document.createElement(\"select\"); select.setAttribute(\"name\",\"Ing\");";
-foreach($ing as $gred){
-    echo "\nvar opt$gred = document.createElement(\"option\"); opt$gred.setAttribute(\"value\",\"$gred\"); var wr$gred = document.createTextNode(\"$gred\"); opt$gred.appendChild(wr$gred); select.appendChild(opt$gred);";
-}
-echo "\nvar grams = document.createElement(\"input\"); grams.setAttribute(\"name\",\"mass\"); grams.setAttribute(\"style\",\"width:200px\"); var sub = document.createElement(\"input\"); sub.setAttribute(\"type\",\"submit\"); sub.setAttribute(\"value\",\"add\"); form.appendChild(select); form.appendChild(grams); form.appendChild(sub); hid.appendChild(form); div.appendChild(hid);};";
+echo "function clickMet(){var menu = document.getElementById(\"mic\"); menu.removeAttribute(\"onclick\"); var select = document.createElement(\"select\");"
 
-echo "function clickMet() {var div = document.getElementById(\"jac\"); var hid = document.createElement(\"div\"); hid.setAttribute(\"style\",\"float:left;width:230px;height:100px\"); var form = document.createElement(\"form\"); form.setAttribute(\"method\",\"post\"); form.setAttribute(\"action\",\"http://jheino.rop.launchbrigade.com/infotech/\"); var select = document.createElement(\"select\"); select.setAttribute(\"name\",\"Ing\");";
-foreach($met as $gred){
-    echo "\nvar opt$gred = document.createElement(\"option\"); opt$gred.setAttribute(\"value\",\"$gred\"); var wr$gred = document.createTextNode(\"$gred\"); opt$gred.appendChild(wr$gred); select.appendChild(opt$gred);";
-}
-echo "\nvar sub = document.createElement(\"input\"); sub.setAttribute(\"type\",\"submit\"); sub.setAttribute(\"value\",\"add\"); form.appendChild(select); form.appendChild(sub); hid.appendChild(form); div.appendChild(hid);};";
+foreach($met as $step){
+    echo "var opt$step = document.createElement(\"option\"); opt$step.setAttrute(\"value\",\"$step\"); var wr$step = document.createTextNode(\"$step\"); opt$step.appendChild(\"wr$step\") select.appendChild(\"op$step\");";
+ }
+
+echo "input = document.createElement(\"input\"); input.setAttribute(\"type\",\"submit\"); input.setAttribrute(\"onclick\",\"sub()\");};";
 
 echo "</script>";
 	
