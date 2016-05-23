eeaa3a44cb1d35ae47357b766542f2da1e69efbb
hthree <jheino@g04.cruziohost.com>
2016-05-23 12:10:51 -0700

    might have broken it

--- templates/index.php
+++ templates/index.php
@@ -3,12 +3,18 @@
 <body>
 <div style ="width:100%;height:100%">
 <div style="width:750px;height:400px;align:center;">
+    <style>
+        .ingdiv{
+            float:left;width:230px;height:100px
+        }
+    </style>
 <?php
 
 // Just grab whatever data was stored in the template
 $rec = $this->recipe;
 $ing = $this->ingredients;
 $met = $this->methods;
+$js = $this->Java;
 echo "<div style=\"width:250px;height:400px;overflow:auto;float:left;\">";
 foreach($rec as $step){
 	echo "<div style=\"float:left;width:230px;height:100px\">$step</div>";
@@ -19,20 +25,43 @@ echo "</div><div style=\"width:250px;height:100px;float:left;\">";
 echo "<div style=\"width:250px;height:100px;float:left;\" id=\"mic\" onclick = \"clickMet()\">add Step</div>";
 //6zjv193
 
-echo "<div style=\"width:250px;height:300px;overflow:auto;float:left;\" id=\"tic\">";
+echo "</div></div><div style=\"width:250px;height:300px;overflow:auto;float:left;\" id=\"tic\">";
 echo "</div><div style=\"width:250px;height:100px;float:left;\">";
 echo "<div style=\"width:250px;height:100px;float:left;\" id=\"mic\" onclick = \"clickIng()\">add ingredient</div>";
-echo "</div></div>";
 	
 echo "<script>\n"; 
+    echo "var js = array(); var ingnum = 0; ingar = array(); function adding(ing){ingar[ingnum] = ing; ingdiv(ingnum); ingnum = ingnum + 1;}; function run(h){ switch(h) {"; 
 //echo"document.getElementById(\"ing\").onclick = function() {clickIng()};document.getElementById(\"met\").onclick = function() {clickMet()};";
-echo "function clickMet(){var menu = document.getElementById(\"mic\"); menu.removeAttribute(\"onclick\"); var select = document.createElement(\"select\");"
+echo "function ingdiv(hhh){var hing = createTextNode(hhh); jsobj.appendChild(hing);};";
+$x = 0;
+foreach($js as $key => $script){
+    echo "case $key: $script break;";
+    $x = $x + 1;
+}
+echo "} };var stepnum = 0;";
+$x = 0;
+foreach($js as $key => $script){
+echo "js[$x] = $key;"; 
+ $x = $x + 1;
+}
+echo "\nfunction clickMet(){var menu = document.getElementById(\"mic\"); menu.removeAttribute(\"onclick\"); menu.removeChild(menu.firstChild); var select = document.createElement(\"select\");";
 
 foreach($met as $step){
-    echo "var opt$step = document.createElement(\"option\"); opt$step.setAttrute(\"value\",\"$step\"); var wr$step = document.createTextNode(\"$step\"); opt$step.appendChild(\"wr$step\") select.appendChild(\"op$step\");";
+    echo "\nvar opt$step = document.createElement(\"option\"); \nopt$step.setAttribute(\"value\",\"$step\"); \nvar wr$step = document.createTextNode(\"$step\"); \nopt$step.appendChild(wr$step); \nselect.appendChild(opt$step);";
  }
 
-echo "input = document.createElement(\"input\"); input.setAttribute(\"type\",\"submit\"); input.setAttribrute(\"onclick\",\"sub()\");};";
+echo "\ninput = document.createElement(\"input\"); input.setAttribute(\"type\",\"submit\"); input.setAttribute(\"onclick\",\"sub(select.value)\"); input.setAttribute(\"value\",\"add\");  menu.appendChild(input); menu.appendChild(select);};";
+echo "\n function sub(thing){var jsobj = document.createElement(\"div\"); dig.setAttribute(\"class\",\"ingdiv\"); run(thing); document.getElementById(\"tic\").appendChild(dig);};";
+
+echo "\nfunction clickIng(){var menu = document.getElementById(\"tic\"); menu.removeAttribute(\"onclick\"); menu.removeChild(menu.firstChild); var select = document.createElement(\"select\");";
+
+foreach($ing as $step){
+    echo "\nvar opt$step = document.createElement(\"option\"); \nopt$step.setAttribute(\"value\",\"$step\"); \nvar wr$step = document.createTextNode(\"$step\"); \nopt$step.appendChild(wr$step); \nselect.appendChild(opt$step);";
+ }
+
+echo "\ninput = document.createElement(\"input\"); input.setAttribute(\"type\",\"submit\"); input.setAttribute(\"onclick\",\"sub(select.value)\"); input.setAttribute(\"value\",\"add\");  menu.appendChild(input); menu.appendChild(select);};";
+echo "\n function sub(thing){var jsobj = document.createElement(\"div\"); dig.setAttribute(\"class\",\"ingdiv\"); adding(thing); document.getElementById(\"tic\").appendChild(dig);};";
+
 
 echo "</script>";
 	
